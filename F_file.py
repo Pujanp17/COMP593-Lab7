2659ea8ff907e664d7d1cd1fb8d99a012b5be56e
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-08-04 10:01:34 +0530

    script 1
    sucess

--- F_file.py
+++ F_file.py
@@ -1,8 +1,32 @@
+from string import digits
 from faker import Faker
-# Create a faker object for English Canadian locale
-fake = Faker("en_CA")
-# Generate fake data for 10 provinces
-for _ in range(10):
-    province = fake.administrative_unit()
-    population = fake.random_int(min=900000, max=100000000)
-    print(f'The population of {province} is {population}.')
+import sqlite3
+from datetime import datetime
+
+fake_data = Faker()
+
+name = fake_data.name();
+address = fake_data.address();
+email = fake_data.email();
+age = fake_data.random_number(digits=2);
+date = fake_data.date_this_month()
+
+#profile = fake_data.simple_profile()
+#for K in range (0,200):
+ #   print(K)
+
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS table1 (name text, address text, email text, age integer, date text)''')
+cur.execute('INSERT INTO table1 VALUES (:name, :address, :email, :age, :date )',
+            {'name': fake_data.name(), 'address' : fake_data.address(), 'email' : fake_data.email(), 'age' : fake_data.random_number(digits=2), 
+                'date' : fake_data.date_this_month()}
+        )
+cur.execute('SELECT * FROM table1')
+
+for i in cur.execute('SELECT name, address, email, age, date FROM table1'):
+    print(i)
+
+con.commit()
+con.close()
